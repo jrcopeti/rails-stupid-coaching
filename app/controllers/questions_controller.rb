d1aef34b9a1cfc34edc8d4b61c7b007343cce330
Jose Copeti <jrcopeti@gmail.com>
2023-01-19 00:03:48 +0100

    challenge completed

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,21 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    coach
+  end
+
+  def coach
+    @answer =
+      if @question == 'I am going to work'
+        'Great!'
+      elsif @question.include? '?'
+        'Silly question, get dressed and go to work!'
+      else
+        'I do not care, get dressed and go to work!'
+      end
+  end
+end
